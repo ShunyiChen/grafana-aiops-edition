2834fdc25acfc9325ebf8a6819a820a86949dc38
526637060@qq.com <526637060@qq.com>
2026-02-21 14:38:49 +0800

    feat:Add a new button 'AIChatTopBarButton' to top toolbar

--- public/app/core/components/AppChrome/TopBar/SingleTopBar.tsx
+++ public/app/core/components/AppChrome/TopBar/SingleTopBar.tsx
@@ -22,6 +22,7 @@ import { NavToolbarSeparator } from '../NavToolbar/NavToolbarSeparator';
 import { QuickAdd } from '../QuickAdd/QuickAdd';
 
 import { HelpTopBarButton } from './HelpTopBarButton';
+import { AIChatTopBarButton } from './AIChatTopBarButton';
 import { InviteUserButton } from './InviteUserButton';
 import { ProfileButton } from './ProfileButton';
 import { SignInLink } from './SignInLink';
@@ -92,10 +93,16 @@ export const SingleTopBar = memo(function SingleTopBar({
           data-testid={!showToolbarLevel ? Components.NavToolbar.container : undefined}
         >
           <TopBarExtensionPoint />
+          
           <TopSearchBarCommandPaletteTrigger />
           {!isSmallScreen && <QuickAdd />}
+          
+          <AIChatTopBarButton isSmallScreen={isSmallScreen} />
+          <NavToolbarSeparator />
+
           <HelpTopBarButton isSmallScreen={isSmallScreen} />
           <NavToolbarSeparator />
+
           {!isSmallScreen && <ExtensionToolbarItem compact={isSmallScreen} />}
           {!showToolbarLevel && actions}
           {!contextSrv.user.isSignedIn && <SignInLink />}
